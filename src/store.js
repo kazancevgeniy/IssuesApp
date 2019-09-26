7c0c93d693f8f0db403d08647be6755a6b7858cb
Evgeniy Kazantsev <ekazancev1922@gmail.com>
2019-09-26 14:46:56 +0300

    add issues loading

--- src/store.js
+++ src/store.js
@@ -1,16 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    issues: []
   },
-  mutations: {
 
+  mutations: {
+    'SET_ISSUES': (state, data) => Vue.set(state, 'issues', [...data])
   },
   actions: {
+    async loadIssues({commit}) {
+      const { data } = await axios.get('https://api.github.com/repos/vuejs/vue/issues')
+      commit('SET_ISSUES', data);
+    }
+  },
 
+  getters: {
+    getIssues: (state) => state.issues
   }
 })
--- src/views/Issues.vue
+++ src/views/Issues.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-        <h1>Issues</h1>
+    <div class="issues">
+        <div class="issues__list-wrapper">
+            <ul class="issues__list">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+
 export default {
-    
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        ...mapGetters(['getIssues'])
+    },
+    created() {
+        this.loadIssues()
+    },
+    methods: {
+        ...mapActions(['loadIssues'])
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+    .issues {
+        width: 100%;
 
+        &__list-wrapper {
+            width: 50%;
+            margin: 0 auto;
+        }
+    }
 </style>
