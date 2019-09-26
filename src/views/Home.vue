c9b1365ce55fb3d5ab9d4a58e5028e944e7cd25f
Evgeniy Kazantsev <ekazancev1922@gmail.com>
2019-09-26 13:48:31 +0300

    add homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="home">
+        <p class="home__description">
+            This is homepage, click 
+            <router-link to="/issues" class="home__description-link">Issues</router-link> 
+            to see open issues
+        </p>
     </div>
 </template>
 
@@ -11,5 +15,23 @@ export default {
 </script>
 
 <style lang="scss">
+    .home {
+        width: 100%;
+        padding-top: 100px;
 
+        &__description {
+            width: 50%;
+            margin: 0 auto;
+            padding: 20px;
+            text-align: center;
+            font-size: 22px;
+            border: 1px dashed grey;
+
+            &-link {
+                color: blue;
+                cursor: pointer;
+                text-decoration: none;
+            }
+        }
+    }
 </style>
